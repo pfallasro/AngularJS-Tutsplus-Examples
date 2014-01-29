cd29c8d43613c812c0c6c332977849b25e909c2a
pfallasro <pfallasro@gmail.com>
2014-01-28 21:29:00 -0600

    adding new functionalities to route example 6

--- E7/contacts.js
+++ E7/contacts.js
@@ -1,5 +1,5 @@
 angular
-	.module('contacts',[])
+	.module('contacts',['ngRoute'])
 	.config(function ($routeProvider){
 		//Configure the routes
 		$routeProvider
@@ -7,6 +7,16 @@ angular
 			.when('/contact/:index', {
 				templateUrl:'edit.html',
 				controller:'Edit'
+			})
+			//Add contact
+			.when('/add', {
+				templateUrl:'edit.html',
+				controller:'Add'
+			})
+			//Delete contact
+			.when('/delete/:index', {
+				templateUrl:'edit.html',
+				controller:'Delete'
 			})
 			.when('/', {
 				//List all contacts
@@ -26,4 +36,16 @@ angular
 		//Load in a contact from the route (/contact/:index)
 		$scope.contact = $scope.contacts[$routeParams.index];
 		$scope.index = $routeParams.index;
+	})
+	.controller('Add', function($scope, $routeParams){
+		var length = $scope.contacts.push({
+			name: 'New contact',
+			number: ''
+		});
+		$scope.contact = $scope.contacts[length - 1];
+		$scope.index = length - 1;
+	})
+	.controller('Delete', function($scope, $routeParams, $location){
+		$scope.contacts.splice($routeParams.index,1);
+		$location.path('/').replace();
 	});
